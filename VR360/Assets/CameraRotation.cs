9c9e4d649de3eb5f339b6186b5f0f65fff479500
J John <Jsmjohn@hotmail.com>
2020-12-18 09:22:49 +0100

    Camera Rotation Test V1

---
+++ VR360/Assets/CameraRotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private Gyroscope gyro;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ModifyGyro();
+    }
+
+    public void ModifyGyro()
+    {
+        transform.rotation = GyroToUnity(gyro.attitude);
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
